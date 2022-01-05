4179e3017b73a72ec399aa0d5b80a688824523dc
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-01-05 21:51:20 +0800

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,5 +1,5 @@
 const HOST = {
-    AUTH_LOGIN = "http://119.29.13.11:3000/auth/login.html",
+    AUTH_LOGIN = "http://119.29.13.11:32000/auth/login.html",
     AUTH_INFO = "http://119.29.13.11:32000/auth/info",
 }
 
