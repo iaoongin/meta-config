f0d3824ed0c9302f43c3157fb8159e0efd6e8a17
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-01-05 22:04:37 +0800

    Update config.js

--- web/config.js
+++ web/config.js
@@ -1,3 +1,4 @@
+// cdn https://cdn.jsdelivr.net/gh/iaoongin/meta-config@main/web/config.js
 const HOST = {
     AUTH_LOGIN = "http://119.29.13.11:32000/auth/login.html",
     AUTH_INFO = "http://119.29.13.11:32000/auth/info",
