ff2a09179831b0986b104d4728246a6b7db31b21
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-05-18 17:37:07 +0800

    Update config.js
    https://auth.meta.yffjglcms.com/

--- web/config.js
+++ web/config.js
@@ -2,7 +2,7 @@
 //     https://purge.jsdelivr.net/gh/iaoongin/meta-config@main/web/config.js
 
 var HOST = {
-    AUTH_LOGIN: "http://119.29.13.11:32000/auth/login.html",
-    AUTH_INFO: "http://119.29.13.11:32000/auth/info",
-    AUTH_CHECK: "http://119.29.13.11:32000/auth/check",
+    AUTH_LOGIN: "https://auth.meta.yffjglcms.com/auth/login.html",
+    AUTH_INFO: "https://auth.meta.yffjglcms.com/auth/info",
+    AUTH_CHECK: "https://auth.meta.yffjglcms.com/auth/check",
 }
